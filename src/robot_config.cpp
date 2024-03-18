f595e6b07f8775939a364305f3ff357cc17483fe
harshad_zade <zade@jpl.nasa.gov>
2024-03-18 19:38:12 -0400

    Add source code for config file and gtest.

---
+++ src/robot_config.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include "manipulator_kinematics/robot_config.h"
+
+// Constructor
+RobotConfig::RobotConfig(const std::string& config_path)
+{
+  loadConfiguration(config_path);
+}
+
+// Load and parse the YAML configuration file
+void RobotConfig::loadConfiguration(const std::string& config_path)
+{
+  YAML::Node config;
+  try
+  {
+    config = YAML::LoadFile(config_path);
+    std::cout << "Loaded configuration file successfully" << std::endl;
+  }
+  catch (YAML::Exception& e)
+  {
+    std::cerr << "Error loading configuration file: " << e.what() << std::endl;
+    throw;
+  }
+  try
+  {
+    for (const auto& link : config["robot"]["links"])
+    {
+      parseLink(link);
+    }
+    for (const auto& joint : config["robot"]["joints"])
+    {
+      parseJoint(joint);
+    }
+    setNumLinks(config["robot"]);
+  }
+  catch (YAML::Exception& e)
+  {
+    std::cerr << "Error parsing either links or joints from the configuration file: " << e.what() << std::endl;
+    throw;
+  }
+}
+
+// Accessor for links
+const std::vector<Link>& RobotConfig::getLinks() const
+{
+  return links_;
+}
+
+// Accessor for joints
+const std::vector<Joint>& RobotConfig::getJoints() const
+{
+  return joints_;
+}
+
+// Accessor for number of links
+const int RobotConfig::getNumLinks() const
+{
+  return num_links_;
+}
+
+// Parse a single link from the YAML node
+void RobotConfig::parseLink(const YAML::Node& node)
+{
+  Link link;
+  link.id = node["id"].as<int>();
+  link.length = node["length"].as<double>();
+  links_.push_back(link);
+}
+
+// Parse a single joint from the YAML node
+void RobotConfig::parseJoint(const YAML::Node& node)
+{
+  Joint joint;
+  joint.id = node["id"].as<int>();
+  joint.theta_min = node["theta_min"].as<double>();
+  joint.theta_max = node["theta_max"].as<double>();
+  joints_.push_back(joint);
+}
+
+// Set the number of links in the robot
+void RobotConfig::setNumLinks(const YAML::Node& node)
+{
+  num_links_ = node["num_links"].as<int>();
+}
---
+++ tests/robot_config_test.cpp
@@ -0,0 +1,35 @@
+#include <gtest/gtest.h>
+#include <cmath>
+#include <fstream>
+#include "manipulator_kinematics/robot_config.h"
+
+auto test_config_path = "../tests/data/test_robot_config_2R.yaml";
+
+// Test for successful loading of configuration file
+TEST(RobotConfigTest, LoadsConfigurationSuccessfully)
+{
+  RobotConfig robotConfig(test_config_path);
+  const auto& links = robotConfig.getLinks();
+  const auto& joints = robotConfig.getJoints();
+  const auto num_links = robotConfig.getNumLinks();
+
+  // Verification for links
+  ASSERT_EQ(links.size(), 2);
+  EXPECT_EQ(links[0].id, 0);
+  EXPECT_DOUBLE_EQ(links[1].length, 5.0);
+
+  // Verification for joints
+  ASSERT_EQ(joints.size(), 2);
+  EXPECT_EQ(joints[0].id, 0);
+  EXPECT_EQ(joints[1].theta_min, -180);
+  EXPECT_EQ(joints[1].theta_max, 180);
+
+  // Verification for number of links
+  EXPECT_EQ(num_links, 2);
+}
+
+int main(int argc, char** argv)
+{
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
